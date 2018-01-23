cbe55007ec03583684e49a198dacaa7284c8177f
Zac-L <Zachlichens@gmail.com>
2018-01-22 16:44:39 -0800

    imported, model, schema, resolver, added graphql tools

--- src/lib/app.mjs
+++ src/lib/app.mjs
@@ -1,18 +1,22 @@
 import express from 'express';
+const app = express();
 import morgan from 'morgan';
 import apolloServer from 'apollo-server-express';
 const { graphqlExpress, graphiqlExpress } = apolloServer;
-// import bodyParser from 'body-parser';
+import graphqlTools from 'graphql-tools';
+const { makeExecutableSchema } = graphqlTools;
+import typeDefs from './models/schema';
+import resolvers from './resolvers';
+import Cat from './models/cat';
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
 
 
-
-const app = express();
-
-const myGraphQLSchema = // ... define or import your schema here!
-
-// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
-app.use('/graphql', express.json(), graphqlExpress({ schema: myGraphQLSchema }));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+app.use('/graphql', express.json(), graphqlExpress({ schema, context: { Cat } }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.use(morgan('dev'));
 
 export default app;
